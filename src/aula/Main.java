9bfb638b21d28004c677ff73022e17d343b1631a
Halerson Rafael <halersoncs@gmail.com>
2021-03-26 10:36:35 -0300

    Variáveis no java

--- src/aula/Main.java
+++ src/aula/Main.java
@@ -3,6 +3,19 @@ package aula;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+       int idade = 24;
+       System.out.println("Minha idade é "+idade);
+       idade = 25;
+        System.out.println("Minha idade é "+idade);
+        idade = 26;
+        System.out.println("Minha idade é "+idade);
+        idade = 27;
+        System.out.println("Minha idade é "+idade);
+        idade = 28;
+        System.out.println("Minha idade é "+idade);
+        idade = 29;
+        System.out.println("Minha idade é "+idade);
+        idade = 30;
+        System.out.println("Minha idade é "+idade);
     }
 }

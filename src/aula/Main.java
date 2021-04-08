a4e6422b11471f405ca361a63cd69eecd3ad91d7
Halerson Rafael <halersoncs@gmail.com>
2021-04-08 09:48:49 -0300

    Constantes

--- src/aula/Main.java
+++ src/aula/Main.java
@@ -20,17 +20,24 @@ public class Main {
         System.out.println("Minha idade é "+idade);
          */
         // Tipos de variáveis;
+        /*
         int numero = 24000000;
         String frase = "Olá mundo";
         boolean var = true;
         char var2 = 'a';
         double var3 = 24.9;
 
+
+
         System.out.println(numero);
         System.out.println(frase);
         System.out.println(var);
         System.out.println(var2);
         System.out.println(var3);
+        */
+        //Constantes
+        final int vida_maxima = 10;
 
+        System.out.println(vida_maxima);
     }
 }

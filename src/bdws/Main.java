3e646372a68c3545919a36b264406a1af3d01af4
halerson <halersoncs@gmail.com>
2021-12-24 10:17:12 -0300

    Gerador número primo básico

--- src/bdws/Main.java
+++ src/bdws/Main.java
@@ -2,6 +2,28 @@ package bdws;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Projeto zerado na branch main");
+        geradorNumeroPrimoBasico();
+    }
+
+    private static void geradorNumeroPrimoBasico() {
+        for (int i = 0; i < 100; i++) {
+            if (isNumeroPrimoBasico(i)){
+                System.out.println(i);
+            }
+        }
+    }
+
+    private static boolean isNumeroPrimoBasico(int numero) {
+        if (numero < 2) {
+            return false;
+        }
+        int metadeNumero = numero / 2;
+        int dividiu = 0;
+        for (int i = 2; i <= metadeNumero ; i++) {
+            if (numero%i ==0){
+                dividiu++;
+            }
+        }
+        return dividiu <= 0;
     }
 }

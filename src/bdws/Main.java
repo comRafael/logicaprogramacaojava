685651679834233cb5f042edc3ff0ea0dae73493
halerson <halersoncs@gmail.com>
2021-12-23 22:56:43 -0300

    Início jakenpo orientado a objeto

--- src/bdws/Main.java
+++ src/bdws/Main.java
@@ -2,6 +2,20 @@ package bdws;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Projeto zerado na branch main");
+        Papel papel = new Papel();
+        Tesoura tesoura = new Tesoura();
+        Pedra pedra = new Pedra();
+
+        Juiz juiz = new Juiz();
+
+        Jogada compara = juiz.comparar(papel, tesoura);
+        juiz.comparar(pedra, tesoura);
+        juiz.comparar(tesoura, papel );
+
+//        boolean ganhou = papel.ganhou();
+//
+//
+//        Jogada jogada = new Papel();
+        System.out.println("Fim");
     }
 }

9e00552356edb6bd5fc6f9dd19ec50dd06afc6eb
halerson <halersoncs@gmail.com>
2022-01-16 17:18:37 -0300

    Frame e Canvas

--- src/bdws/Main.java
+++
@@ -1,7 +0,0 @@
-package bdws;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Projeto zerado na branch main");
-    }
-}
---
+++ src/pack/Game.java
@@ -0,0 +1,32 @@
+package pack;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable {
+    public static JFrame frame;
+    private final int WIDTH = 160;
+    private final int HEIGHT = 120;
+    private final int SCALE = 4;
+
+
+    public Game(){
+        this.setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
+        frame = new JFrame();
+        frame.add(this);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+
+
+    public void run() {
+
+    }
+}

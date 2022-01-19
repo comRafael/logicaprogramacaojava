7dc74eefcdcd80dd1820e72bd21451e0275e152d
halerson <halersoncs@gmail.com>
2022-01-19 19:15:57 -0300

    Game Looping profissional

--- src/pack/Game.java
+++ src/pack/Game.java
@@ -5,6 +5,8 @@ import java.awt.*;
 
 public class Game extends Canvas implements Runnable {
     public static JFrame frame;
+    private Thread thread;
+    private boolean isRunning = true;
     private final int WIDTH = 160;
     private final int HEIGHT = 120;
     private final int SCALE = 4;
@@ -26,15 +28,53 @@ public class Game extends Canvas implements Runnable {
     }
 
     public synchronized void start(){
+        thread = new Thread(this);
+        isRunning = true;
+        thread.start();
+    }
+
+    public synchronized void stop(){
 
     }
 
     public static void main(String[] args) {
         Game game = new Game();
+        game.start();
+    }
+
+    public void tick(){
+
+    }
+
+    public void render (){
+
     }
 
 
     public void run() {
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta  = 0;
+        int frames = 0;
+        double timer = System.currentTimeMillis();
+        while(isRunning){
+            long now = System.nanoTime();
+            delta+= (now - lastTime) / ns;
+            lastTime = now;
 
+            if (delta >= 1) {
+                tick();
+                render();
+                frames++;
+                delta--;
+            }
+
+            if(System.currentTimeMillis()- timer >= 1000){
+                System.out.println("FPS: " + frames);
+                frames = 0;
+                timer+=1000;
+            }
+        }
     }
 }

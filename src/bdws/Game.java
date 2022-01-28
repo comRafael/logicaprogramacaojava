59af3515751c2fbe973c88e8699d6de5611a8acf
halerson <halersoncs@gmail.com>
2022-01-28 12:42:35 -0300

    Criando a Janela do Pong Colocando o JFrame para criar a janela do nosso game, e implementando o Runnable

---
+++ src/bdws/Game.java
@@ -0,0 +1,29 @@
+package bdws;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends Canvas implements Runnable {
+
+    public static int WIDTH = 240;
+    public static int HEIGHT = 120;
+    public static int SCALE = 3;
+
+    public Game(){
+        this.setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
+    }
+
+    public static void main(String[] args) {
+        Game game =  new Game();
+        JFrame frame = new JFrame("Pong");
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(game);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    public void run() {
+
+    }
+}

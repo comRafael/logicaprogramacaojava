54a52f601b6c2a31fbcd598e4fbaac7eaf3a05f5
halerson <halersoncs@gmail.com>
2022-01-29 11:34:36 -0300

    Colocando movimento com teclas, e colisão no jogador

--- src/bdws/Game.java
+++ src/bdws/Game.java
@@ -2,16 +2,28 @@ package bdws;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
 
-public class Game extends Canvas implements Runnable {
+public class Game extends Canvas implements Runnable, KeyListener {
 
     public static int WIDTH = 240;
     public static int HEIGHT = 120;
     public static int SCALE = 3;
 
+    public BufferedImage layer = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
+
+    public Player player;
+
     public Game(){
         this.setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
+        this.addKeyListener(this);
+        player = new Player(100,HEIGHT-5);
     }
+
 
     public static void main(String[] args) {
         Game game =  new Game();
@@ -22,8 +34,64 @@ public class Game extends Canvas implements Runnable {
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        new Thread(game).start();
     }
-    public void run() {
 
+    public void tick(){
+        player.tick();
+    }
+
+    public void render(){
+        BufferStrategy bs = this .getBufferStrategy();
+        if (bs == null){
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics graphics = layer.getGraphics();
+        graphics.setColor(Color.black);
+        graphics.fillRect(0,0,WIDTH,HEIGHT);
+        player.render(graphics);
+        graphics = bs.getDrawGraphics();
+        graphics.drawImage(layer,0,0,WIDTH*SCALE,HEIGHT*SCALE,null);
+        bs.show();
+    }
+
+    public void run() {
+        while (true){
+            tick();
+            render();
+            try {
+                Thread.sleep(1000/60);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            player.right = true;
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            player.left = true;
+        }
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            player.right = false;
+        }
+        else if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            player.left = false;
+        }
     }
 }
